aeab878549806d35329017b2105a28e81eca7bb8
Mariusz Perzynski <m.perzyk@interia.pl>
2021-07-04 11:48:51 +0200

    update and close the CL

--- image.py
+++ image.py
@@ -4,12 +4,4 @@
 
 import pandas as pd
 
-########### SERIES ################
-ex = pd.Series([-2,-1,5])
-ex * 10
-ex.abs()
-ex.describe()
-ex.index["HELLO ","ELO ","LLO "]
-ex["HELLO"]
-
 ########### DataFrame #############
